45e847c9d5b72ce431abbe4abca99585303eaea2
farrisfoo <foo.farris@gmail.com>
2026-07-01 00:52:11 +0800

    finalise payment method function

--- resources/views/payment-methods.blade.php
+++ resources/views/payment-methods.blade.php
@@ -230,7 +230,7 @@
             </div>
         </div>
     </div>
-    <!-- Bootstrap JS for modals -->
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
     <!-- Image Preview Script -->
